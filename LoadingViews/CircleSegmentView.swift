f0b39bcc36850b2d9bf3cc9ba87a15d7f41a3297
Dalton Claybrook <daltonclaybrook@gmail.com>
2017-02-12 23:50:51 -0600

    Now more customizable

--- LoadingViews/CircleSegmentView.swift
+++ LoadingViews/CircleSegmentView.swift
@@ -10,14 +10,23 @@ import UIKit
 
 class CircleSegmentView: UIView {
     
+    enum MaskStyle {
+        case outward, inward
+    }
+    
     var isAnimating = false
+    var lineWidthRange: ClosedRange<CGFloat> = (20...60)
+    var segmentLengthRange: ClosedRange<CGFloat> = (0.05...0.1) { didSet { configureLayout() } }
+    var rotationDuration: CFTimeInterval = 7.5 { didSet { configureLayout() } }
+    var maskStyle = MaskStyle.outward { didSet { configureLayout() } }
+    
     private var shapeLayers = [CAShapeLayer]()
     override var bounds: CGRect {
         didSet { configureLayout() }
     }
     
     override var intrinsicContentSize: CGSize {
-        let size: CGFloat = 60
+        let size: CGFloat = 100
         return CGSize(width: size, height: size)
     }
     
@@ -55,11 +64,34 @@ class CircleSegmentView: UIView {
         createShapeLayersIfNecessary()
         let path = UIBezierPath(ovalIn: bounds)
         shapeLayers.forEach { $0.path = path.cgPath }
+        layer.mask = maskStyle == .outward ? createOutwardMask() : createInwardMask()
         createRotateAnimation()
     }
     
+    private func createOutwardMask() -> CALayer {
+        let path = UIBezierPath(rect: CGRect(x: -10000, y: -10000, width: 20000, height: 20000))
+        path.append(UIBezierPath(ovalIn: bounds))
+        path.usesEvenOddFillRule = true
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.fillColor = UIColor.green.cgColor
+        maskLayer.strokeColor = nil
+        maskLayer.path = path.cgPath
+        maskLayer.fillRule = kCAFillRuleEvenOdd
+        return maskLayer
+    }
+    
+    private func createInwardMask() -> CALayer {
+        let path = UIBezierPath(ovalIn: bounds)
+        
+        let maskLayer = CAShapeLayer()
+        maskLayer.fillColor = UIColor.green.cgColor
+        maskLayer.strokeColor = nil
+        maskLayer.path = path.cgPath
+        return maskLayer
+    }
+    
     private func createAllAnimations() {
-//        createAnimation(for: shapeLayers.first!, fromValue: shapeLayers.first!.lineWidth)
         shapeLayers.forEach { self.createAnimation(for: $0, fromValue: $0.lineWidth) }
     }
     
@@ -83,16 +115,18 @@ class CircleSegmentView: UIView {
         let animation = CABasicAnimation(keyPath: "transform.rotation.z")
         animation.fromValue = 0.0
         animation.toValue = CGFloat.pi * 2.0
-        animation.duration = 10.0
+        animation.duration = rotationDuration
         animation.repeatCount = .greatestFiniteMagnitude
-        layer.add(animation, forKey: nil)
+        
+        layer.removeAnimation(forKey: "rotation")
+        layer.add(animation, forKey: "rotation")
     }
     
     private func createShapeLayersIfNecessary() {
         guard shapeLayers.count == 0 else { return }
         var lengthRemaining: CGFloat = 1.0
         while lengthRemaining > 0.0 {
-            var length = CGFloat(arc4random_uniform(50) + 50) / 1000.0 // 0.05 - 0.1
+            var length = randomSegmentLength()
             length = min(length, lengthRemaining)
             let epsilon: CGFloat = 0.005
             
@@ -111,6 +145,13 @@ class CircleSegmentView: UIView {
     }
     
     private func randomLineWidth() -> CGFloat {
-        return CGFloat(arc4random_uniform(25) + 5)
+        return CGFloat(arc4random_uniform(UInt32(lineWidthRange.upperBound-lineWidthRange.lowerBound))) + lineWidthRange.lowerBound
+    }
+    
+    private func randomSegmentLength() -> CGFloat {
+        let largeUpper = UInt32(segmentLengthRange.upperBound * 1000)
+        let largeLower = UInt32(segmentLengthRange.lowerBound * 1000)
+        let largeLength = arc4random_uniform(largeUpper-largeLower) + largeLower
+        return CGFloat(largeLength) / 1000.0
     }
 }
